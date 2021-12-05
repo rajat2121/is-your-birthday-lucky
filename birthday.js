580937c3f4dfe4e392042dfcbfd01e97240c7d94
rajat2121 <rajatawasthi99@gmail.com>
2021-12-05 15:12:14 +0530

    birthday.js
    birthday function

---
+++ birthday.js
@@ -0,0 +1,38 @@
+// input
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number")
+const checkBtn = document.querySelector("#check-btn");
+const output = document.querySelector("#output");
+// console.log(userInput)
+
+// processing
+function calSum(dob) {
+    dob = dob.replaceAll("-","")
+    let sum = 0;
+    for(let index=0; index <= dob.length; index++) {
+        sum =  sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+function luckyOrNotBtn() {
+    const dob = dateOfBirth.value;
+    const sum = calSum(dob);
+    if(sum&&dob) {
+    compareValues(sum, Number(luckyNumber.value))
+    } else {
+        output.innerText = "Please enter both VALUES!!"
+    }
+}
+
+checkBtn.addEventListener("click", luckyOrNotBtn);
+
+// output
+
+function compareValues(sum, luckyNumber){
+    if(sum%luckyNumber === 0) {
+        output.innerText = "Your Birthday is LUCKY!"
+    }  else [
+        output.innerText = "Your Birthday is so LUCKY and special!!"
+    ]
+}
